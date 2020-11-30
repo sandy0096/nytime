a87e94be79adc0d54a2c32f141bcd569095be3b8
Sandeep Singh <sandeep.singh@jaarvis.com>
2020-11-30 09:25:46 +0530

    xomm

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ module.exports = {
         URL: 'https://api.nytimes.com/svc/topstories/v2/',
         SEARCH: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
         KEY: 'r8240WTHxEvbAXNGC3LGHWnmwAACG1Sg',
-        PAGE: ['world', 'us', 'politics', 'nyregion', 'business', 'opinion', 'technology', 'science', 'health', 'sports', 'arts', 'books', 'style', 'food', 'travel', 'magazine']
+        PAGE: ['world', 'us', 'politics', 'nyregion', 'business', 'opinion', 'technology', 'science', 'health', 'sports', 'arts', 'books', 'style', 'food', 'travel', 'magazine'],
+        NODE_ENV: 'production'
     },
     webpack: (config, options) => {
         config.module.rules.push({
